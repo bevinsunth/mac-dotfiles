316b15b4b76939580e5a5c731f8e59240473e25c
BevTheDev <iambevthedev@gmail.com>
2026-08-03 15:13:43 +1000

    Minor update

---
+++ .config/nvim/lua/plugins/explorer.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    explorer = {},
+    picker = {
+      sources = {
+        explorer = { layout = { layout = { position = "right" } } },
+      },
+    },
+  },
+}
